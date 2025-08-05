2ce371230cedf8b2eb3ba009fb704b7b0f81ec33
victorian-art <info@victorian-art.ae>
2025-08-05 18:13:24 +0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function(e) {
-  console.log('Service Worker: Installed');
-});
-
-self.addEventListener('fetch', function(e) {
-  e.respondWith(fetch(e.request));
-});
