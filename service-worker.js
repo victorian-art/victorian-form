ea6e257175a5292b83399c3e2c32a1789370e4bb
victorian-art <info@victorian-art.ae>
2025-08-05 16:25:02 +0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('curtain-store').then(function(cache) {
+      return cache.addAll([
+        './',
+        './curtain_app_full_whatsapp.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
